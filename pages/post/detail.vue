a28959fd64ade3d9dcda72f6ae6c22520c2a0482
Monday <97xuye@gmail.com>
2020-02-19 20:22:13 +0800

    完成文章布局

---
+++ pages/post/detail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="postdetail">
+    <el-row type="flex">
+      <!-- 左侧 -->
+      <div class="main">
+        <!-- 文章详情 -->
+        <articleDetail />
+        <!-- 用户评论 -->
+        <div class="usercomment">
+          <div>评论</div>
+          <textarea></textarea>
+        </div>
+      </div>
+
+      <!-- 右侧 -->
+      <div class="about">相关攻略</div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import articleDetail from "@/components/post/articleDetail";
+export default {
+  components: {
+    articleDetail
+  }
+};
+</script>
+
+<style>
+.postdetail {
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
