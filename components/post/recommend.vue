a5752d710a6a40c9f2446aed93132eb618eac27d
Monday <97xuye@gmail.com>
2020-02-20 22:21:10 +0800

    推荐获取渲染完成（图片路径暂未找到）

---
+++ components/post/recommend.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="recommendContainer">
+    <el-row type="flex">
+      <img v-if="recommendData.img" src="@/assets/1.png" alt />
+      <el-row>
+        <div class="up">{{recommendData.title}}</div>
+        <div class="down">{{time}}&nbsp;&nbsp;&nbsp;阅读{{recommendData.watch}}</div>
+      </el-row>
+    </el-row>
+  </div>
+</template>
+
+<script>
+var moment = require('moment');
+export default {
+    props:['recommendData'],
+    data () {
+        return {
+            
+            time:''
+        }
+    },
+    mounted () {
+         let data = this.recommendData.created_at
+        /*let d = new Data(data)
+        let time = moment().format(); */
+        let day = moment(data).format('YYYY-MM-DD');
+       
+        let time = moment(data).format('hh:mm');
+         this.time = day + time
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.recommendContainer {
+  width: 100%;
+  float: right;
+  margin-right: 0;
+  padding: 20px 0;
+  border-bottom: 1px solid #ccc;
+  img {
+    width: 100px;
+    height: 80px;
+  }
+  .up {
+    padding: 0 20px;
+  }
+  .down {
+    padding: 0 20px;
+    margin-top: 30px;
+    margin-bottom: 0;
+    font-size: 12px;
+  }
+}
+</style>
--- pages/post/detail.vue
+++ pages/post/detail.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="postdetail">
-    <el-row type="flex">
+    <el-row type="flex" justify="space-between">
       <!-- 左侧 -->
       <div class="main clearfix">
         <!-- 文章详情 -->
         <articleDetail />
         <!-- 用户输入 -->
-        <comments @addComments="addComments" :commentTitle="'评论'"/>
+        <comments @addComments="addComments" :commentTitle="'评论'" />
         <div style="height:20px"></div>
         <!-- 展示评论 -->
         <commentShow v-for="(item,index) in commnetsdata" :key="index" :commnetsdata="item" />
       </div>
 
       <!-- 右侧 -->
-      <div class="about">相关攻略</div>
+      <!-- 相关攻略 -->
+      <div class="right">
+        <div class="someRecommend">
+          相关攻略
+        </div>
+        <hr>
+        <recommend 
+        v-for="(item,index) in recommendData"
+        :key="index"
+         :recommendData="item"/>
+      </div>
+
+      <!-- <div class="about">相关攻略</div> -->
     </el-row>
   </div>
 </template>
@@ -22,6 +34,7 @@
 import articleDetail from "@/components/post/articleDetail";
 import comments from "@/components/post/comments";
 import commentShow from "@/components/post/commentShow";
+import recommend from "@/components/post/recommend";
 
 export default {
   data() {
@@ -31,19 +44,31 @@ export default {
         {
           account: {}
         }
-      ]
+      ],
+      //推荐文章的数据
+      recommendData:[]
     };
   },
   mounted() {
-    this.init()
-    console.log(12354);
+    this.init();
+     //加载攻略
+     this.$axios({
+       url:'/posts/recommend',
+       params:{
+         id:this.$route.query.id
+       }
+     }).then(res=>{
+       console.log(res);
+       this.recommendData = res.data.data
+     })
   },
   methods: {
     // 添加评论时
-    addComments(){
-      this.init()
+    addComments() {
+      this.init();
     },
     init() {
+      // 加载文章、评论
       this.$axios({
         url: "/posts/comments",
         params: {
@@ -52,9 +77,11 @@ export default {
           _limit: 5
         }
       }).then(res => {
-        console.log(res);
+        // console.log(res);
         this.commnetsdata = res.data.data;
       });
+     
+
     }
   },
   components: {
@@ -63,13 +90,15 @@ export default {
     // 评论输入
     comments,
     // 展示评论
-    commentShow
+    commentShow,
+    //相关推荐
+    recommend
   }
 };
 </script>
 
 <style>
-.main{
+.main {
   /* overflow: hidden; */
 }
 .postdetail {
@@ -83,5 +112,11 @@ export default {
   content: "";
   clear: both;
   display: block;
+}
+.someRecommend{
+  margin-bottom: 20px;
+}
+.right{
+  width:262px;
 }
 </style>
