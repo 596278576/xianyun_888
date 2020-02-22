20a228bbc8200da0f051625f209f29145865f712
change <xjx903697869@163.com>
2020-02-22 19:31:40 +0800

    03-渲染页面,地图未完成

---
+++ components/hotel/detailMap.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="info-row">
+    <div class="map-view">
+      <div id="container" class="map-box"></div>
+      <div class="poi-list">
+        <el-tabs v-model="editableTabsValue" type="border-card" @tab-click="handleSearch">
+          <el-tab-pane label="风景" name="1">
+            <div class="poi-list-ol" id="panel"></div>
+          </el-tab-pane>
+          <el-tab-pane label="交通" name="2">
+            <div class="poi-list-ol" id="traffic"></div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: {
+        location: {}
+      }
+    }
+  },
+  data() {
+    return {
+      centerInfo: [],
+      editableTabsValue: "1",
+      map: ""
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.centerInfo = [
+        this.data.location.longitude,
+        this.data.location.latitude
+      ];
+      console.log(this.centerInfo);
+    }, 100);
+
+    var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=ccfe0710919fd363dbfb8d9b863a8519&callback=onLoad&plugin=AMap.PlaceSearch";
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+    //组件加载完后执行的函数
+    setTimeout(() => {
+      this.mapInit();
+    }, 200);
+  },
+  methods: {
+    //地图初始化
+    mapInit() {
+      window.onLoad = () => {
+        this.map = new AMap.Map("container", {
+          center: this.centerInfo,
+          zoom: 14, //初始地图级别
+          resizeEnable: true
+        });
+        this.searchScenery();
+      };
+    },
+    searchScenery() {
+      AMap.service(["AMap.PlaceSearch"], () => {
+        //构造地点查询类
+        var placeSearch = new AMap.PlaceSearch({
+          pageSize: 10, // 单页显示结果条数
+          pageIndex: 1, // 页码
+          city: this.data.city.name, // 兴趣点城市
+          citylimit: true, //是否强制限制在设置的城市内搜索
+          map: this.map, // 展现结果的地图实例
+          panel: "panel", // 结果列表将在此容器中进行展示。
+          autoFitView: true // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
+        });
+        //关键字查询
+        placeSearch.searchNearBy("风景", this.centerInfo, 5000, function(
+          status,
+          result
+        ) {});
+      });
+    },
+    searchTraffic() {
+      AMap.service(["AMap.PlaceSearch"], () => {
+        //构造地点查询类
+        var placeSearch = new AMap.PlaceSearch({
+          pageSize: 10, // 单页显示结果条数
+          pageIndex: 1, // 页码
+          city: this.data.city.name, // 兴趣点城市
+          citylimit: true, //是否强制限制在设置的城市内搜索
+          map: this.map, // 展现结果的地图实例
+          panel: "traffic", // 结果列表将在此容器中进行展示。
+          autoFitView: true // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
+        });
+        //关键字查询
+        placeSearch.searchNearBy("公交地铁", this.centerInfo, 5000, function(
+          status,
+          result
+        ) {});
+      });
+    },
+
+    handleSearch(tab, event) {
+      // 根据地图的容器生成地图
+      this.map = new AMap.Map("container", {
+        resizeEnable: true
+      });
+      if (tab.label == "风景") {
+        this.searchScenery();
+      }
+      if (tab.label == "交通") {
+        this.searchTraffic();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.info-row {
+  margin: 40px 0;
+}
+.map-view {
+  display: flex;
+  flex: 1;
+  height: 360px;
+  .map-box {
+    width: 65%;
+    height: 100%;
+  }
+  .poi-list {
+    padding-left: 20px;
+    width: 35%;
+    /deep/.el-tabs--border-card > .el-tabs__content {
+      padding: 10px;
+      padding-right: 0px;
+    }
+    .poi-list-ol {
+      height: 300px;
+      overflow: auto;
+      font-size: 14px;
+      color: #666;
+      /deep/.amap_lib_placeSearch {
+        border: 0;
+        /deep/.amap_lib_placeSearch_page {
+          background: #fff;
+        }
+        /deep/.pageLink {
+          border-radius: 20%;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/hotel/detail.vue
+++ pages/hotel/detail.vue
@@ -3,63 +3,33 @@
     <!-- 顶部面包屑 -->
     <div class="header">
       <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/hotel' }">酒店</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/hotel' }">广州酒店</el-breadcrumb-item>
-        <el-breadcrumb-item>广州华尔道夫酒店</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/hotel' }">{{Detail.big_cate}}</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/hotel' }">{{Detail.real_city}}酒店</el-breadcrumb-item>
+        <el-breadcrumb-item>{{Detail.name}}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <!-- 酒店主题 -->
     <el-row class="address">
       <el-col class="el-col el-col-24">
         <h4>
-          广州华尔道夫酒店
-          <span title="星级">
-            <i class="iconfont iconhuangguan"></i>
-            <i class="iconfont iconhuangguan"></i>
-            <i class="iconfont iconhuangguan"></i>
-            <i class="iconfont iconhuangguan"></i>
-            <i class="iconfont iconhuangguan"></i>
-          </span>
+          {{Detail.name}}
+          <span title="星级"></span>
         </h4>
         <!-- 拼音 -->
         <el-row class="row">
-          <span class="hotel-en-name">shang hai wai tan hua er dao fu hotel</span>
+          <span class="hotel-en-name">{{Detail.alias}}</span>
         </el-row>
         <!-- 具体位置 -->
         <el-row class="row">
-          <i class="iconfont iconlocation">中山东一路2号，近广东路</i>
+          <i class="iconfont iconlocation">{{Detail.address}}</i>
         </el-row>
       </el-col>
     </el-row>
 
     <!-- 酒店全景图 -->
-    <el-row class="photo-left">
-      <!-- 左侧图片 -->
-      <el-col class="left el-col el-col-16">
-        <!-- <img
-          height="360"
-          width="640"
-          src="http://157.122.54.189:9095/images/hotel-pics/1.jpeg"
-          alt=" 广州华尔道夫酒店"
-        />-->
-      </el-col>
-      <el-col class="right el-col el-col-8">
-        <!-- 右侧图片 -->
-        <el-row class="photo-right" style="margin-left: -10px; margin-right: -10px;">
-          <el-col class="el-col el-col-12" style="padding-left:10px;padding-right:10px">
-            <a href="javascript:void(0)">
-              <!-- <img
-                width="160px"
-                src="http://157.122.54.189:9095/images/hotel-pics/1.jpeg"
-                alt="广州华尔道夫酒店"
-              />-->
-            </a>
-          </el-col>
-        </el-row>
-      </el-col>
-    </el-row>
+    <HotelImage />
 
-    <!-- 价格 -->
+    <!-- 酒店价格 -->
     <el-row type="flex" class="row-bg" justify="space-between">
       <p>价格来源</p>
       <p>低价房型</p>
@@ -71,130 +41,59 @@
         <span>携程</span>
         <span>高级大床房A</span>
         <div class="grid-content bg-purple">
-          <span>￥239</span>
+          <!-- <span>￥{{ scope.row.price }}</span> -->
           <span>起</span>
-          <a href="https://hotels.ctrip.com/hotel/694679.html">
-            <i class="el-icon-arrow-right height-light"></i>
-          </a>
-        </div>
-      </el-row>
-      <el-row type="flex" class="row-bgc" justify="space-between">
-        <span>携程</span>
-        <span>高级大床房A</span>
-        <div class="grid-content bg-purple">
-          <span>￥239</span>
-          <span>起</span>
-          <a href="https://hotels.ctrip.com/hotel/694679.html">
-            <i class="el-icon-arrow-right height-light"></i>
-          </a>
-        </div>
-      </el-row>
-      <el-row type="flex" class="row-bgc" justify="space-between">
-        <span>携程</span>
-        <span>高级大床房A</span>
-        <div class="grid-content bg-purple">
-          <span>￥239</span>
-          <span>起</span>
-          <a href="https://hotels.ctrip.com/hotel/694679.html">
-            <i class="el-icon-arrow-right height-light"></i>
-          </a>
+          <i class="el-icon-arrow-right height-light"></i>
         </div>
       </el-row>
     </div>
 
     <!-- 地图 -->
-    <AMap />
+    <!-- <DetailMap :data="Detail" /> -->
 
     <!-- 酒店基本信息 -->
-    <el-row type="flex" class="info">
-      <el-col :span="4">基本信息</el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple">入住时间: 14:00之后</div>
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple">离店时间: 12:00之前</div>
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple">2010年开业 / 2010年装修</div>
-      </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple">酒店规模: 153间客房</div>
-      </el-col>
-    </el-row>
-    <!-- 设施 -->
-    <el-col :gutter="20" class="facility">
-      <el-col :span="4">
-        <div class="grid-content bg-purple">基本设施</div>
-      </el-col>
-      <el-col :span="20">
-        <div class="grid-content bg-purple">
-          <i>热水壶</i>
-        </div>
-      </el-col>
-    </el-col>
-    <!-- 服务 -->
-    <el-row class="serve" type="flex">
-      <el-col :span="4">
-        <div class="grid-content bg-purple">停车服务</div>
-      </el-col>
-      <el-col :span="20">
-        <div class="grid-content bg-purple-light">-</div>
-      </el-col>
-    </el-row>
-    <!-- 品牌 -->
-    <el-row type="flex" class="brand">
-      <el-col :span="4">
-        <div class="grid-content bg-purple">品牌信息</div>
-      </el-col>
-      <el-col :span="20">
-        <div class="grid-content bg-purple">
-          <span>贝壳</span>
-        </div>
-      </el-col>
-    </el-row>
+    <DetailInformation :data="Detail" />
 
     <!-- 用户评论 -->
-    <div class="review">
-      <h3>0条真实用户评论</h3>
-      <el-row type="flex">
-        <el-col :span="4">
-          <div class="grid-content bg-purple">
-            <p>总评论数：64</p>
-            <p>好评数：13</p>
-            <p>差评数：5</p>
-          </div>
-        </el-col>
-        <el-col :span="5">
-          <div class="grid-content bg-purple-light">
-            <el-rate show-text></el-rate>
-          </div>
-        </el-col>
-        <el-col :span="3">
-          <div class="grid-content bg-purple"></div>
-        </el-col>
-        <el-col :span="3">
-          <div class="grid-content bg-purple-light"></div>
-        </el-col>
-        <el-col :span="3">
-          <div class="grid-content bg-purple"></div>
-        </el-col>
-      </el-row>
-    </div>
+    <UserReview :data="Detail" />
   </div>
 </template>
 
 <script>
-import AMap from "@/components/hotel/AMap";
+// 酒店图片
+import HotelImage from "@/components/hotel/hotelimg";
+// 用户评论
+import UserReview from "@/components/hotel/userReview";
+// 酒店信息
+import DetailInformation from "@/components/hotel/detailInformation";
+// 酒店地图
+// import DetailMap from "@/components/hotel/detailMap";
 export default {
   components: {
-    AMap
+    HotelImage,
+    // DetailMap,
+    DetailInformation,
+    UserReview
   },
   data() {
-    return {};
+    return {
+      id: 185,
+      Detail: {
+        hotelbrand: {}, //酒店品牌
+        location: {}, //坐标
+        scores: {} //评分
+      }
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: `/hotels?id=` + this.id
+    }).then(res => {
+      console.log(res.data);
+    });
   }
 };
 </script>
-
 <style scoped lang="less">
 .detail {
   margin: 0 auto;
@@ -245,34 +144,5 @@ p {
       color: #f90;
     }
   }
-}
-.info {
-  margin-top: 50px;
-  padding: 20px 10px;
-  color: #000;
-  border-bottom: 1px solid #ccc;
-  .bg-purple {
-    color: #666;
-  }
-}
-.facility {
-  padding: 20px 10px;
-  border-bottom: 1px solid #ccc;
-  /deep/ i {
-    border: 1px solid #eee;
-    border-radius: 4px;
-    color: #666;
-    background-color: #eee;
-    padding: 4px 10px;
-    margin-right: 10px;
-  }
-}
-.serve,
-.brand {
-  padding: 20px 10px;
-  border-bottom: 1px solid #ccc;
-}
-.review {
-  margin: 30px 0;
 }
 </style>
